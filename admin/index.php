cdf4142466fb842f23e8ccd3adb5a505a64b38e1
sandulr <sandulr2@gmail.com>
2025-11-01 02:09:52 +0530

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<?php include 'admin_header.php'; ?>
+
+<h2>Admin Dashboard</h2>
+<ul>
+    <li><a href="manage_users.php">Manage Users</a></li>
+    <li><a href="manage_classes.php">Manage Classes</a></li>
+    <li><a href="manage_trainers.php">Manage Trainers</a></li>
+    <li><a href="view_queries.php">Manage Queries</a></li>
+</ul>
+
+<?php
+
+include '../includes/footer.php';
